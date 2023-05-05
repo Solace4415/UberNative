0ff8c06b6b3baa780f92560fc1fcd923642cf377
Solace4415 <tijjtay@gmail.com>
2023-05-05 08:18:53 +0100

    changes made

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,5 +1,5 @@
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const data = [
   {
@@ -21,10 +21,17 @@ const NavOptions = () => {
     <FlatList
       data={data}
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.title}</Text>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold `}>{item.title}</Text>
+          </View>
         </TouchableOpacity>
       )}
+      horizontal
       keyExtractor={(item) => item.id}
     />
   );
